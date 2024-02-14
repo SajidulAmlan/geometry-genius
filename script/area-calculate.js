d770e887d0701baa61a3847f16f0130b51ab97de
SajidulAmlan <87017974+SajidulAmlan@users.noreply.github.com>
2024-02-14 12:04:19 +0600

    first commit

---
+++ script/area-calculate.js
@@ -0,0 +1,21 @@
+function calculateTriangleArea(){
+    // get
+const triangleBaseInput= document.getElementById('triangle-base');
+const triangleBaseText = triangleBaseInput.value;
+const base = parseFloat( triangleBaseText)
+
+
+const triangleHightInput =document.getElementById('triangle-hight');
+const triangleHightText = triangleHightInput.value;
+const hight = parseFloat( triangleHightText);
+
+const area = .5* base * hight ;
+console.log('Area of the triangle is', area) ;
+
+const triangleAreaSpan = document.getElementById('triangle-area');
+triangleAreaSpan.innerText= area;
+}
+
+function calculateRectangleArea(){
+   const
+}
